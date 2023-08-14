de9c1fad516e8fef088406552e0750335646f95c
Yuliana Montaño <yulianamarcela200@gmail.com>
2023-08-14 08:29:11 -0400

    3 y 4

---
+++ src/clean-code/03-functios.ts
@@ -0,0 +1,8 @@
+  const getPayAmount = ({ isDead = false, isSeparated = true, isRetired = false }) : number => {
+        
+        if ( isDead ) return 1500;
+            
+        if ( isSeparated ) return 2500;
+
+        return isRetired ? 3000 : 4000;
+    }
